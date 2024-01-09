55c31ee6ea017e5d7033327504afeaa7b6899569
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-01-09 23:38:09 +0100

    added external js

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+// Get acces to the button
+const button = document.querySelector('button');
+
+// Add event listener to the buton
+button.addEventListener('click', showMyName);
+
+// Function to show my name
+function showMyName(){
+    alert('My name is AbdulGaniyu')
+}
