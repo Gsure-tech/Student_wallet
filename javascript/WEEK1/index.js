fdbf429b37348672e4972f506f3837095d1e0d5d
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-01-16 22:28:44 +0100

    basic operators

--- javascript/WEEK1/index.js
+++ javascript/WEEK1/index.js
@@ -2,14 +2,14 @@
 const button = document.querySelector('button');
 
 // Add event listener to the buton
-button.addEventListener('click', showMyName);
+// button.addEventListener('click', showMyName);
 
 // Function to show my name
 function showMyName(){
     alert('My name is AbdulGaniyu');
 }
 let message = "Hello my friends\n My name is Aisha\n I am a Medical Doctor"
-alert(message.toUpperCase())
+// alert(message.toUpperCase())
 
 let user2 ={
     name: "John Doe",
@@ -28,9 +28,14 @@ let firstName = `Sani`
 let lastName = `Bello`
 let fullName = `Mr ${firstName} ${lastName}`
 
-alert(fullName)
+// alert(fullName)
 
 console.log(user)
 console.log(message.startsWith("Hello"))
 console.log(user2.verifiedUser);
 console.log(user.weight);
+
+let assignmen = 20;
+let ass = assignmen++
+alert(ass)
+alert(++ass)
