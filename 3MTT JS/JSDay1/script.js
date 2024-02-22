433a7f17c68d458eb1ed7f23bcbd10c4df4d573c
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-22 10:54:13 +0100

    using javascript escape sequence

---
+++ 3MTT JS/JSDay1/script.js
@@ -0,0 +1,23 @@
+
+// 
+// alert('This is an external page')
+
+
+// Multiple Line comment
+/*
+Lorem ipsum, dolor sit amet consectetur adipisicing 
+elit. Eaque odit hic repudiandae at similique atque. Numquam
+harum laborum unde temporibus beatae non, nesciunt est ratione
+ voluptas eos. Quas, perferendis est!
+ */
+
+// alert('It\s raining!')  //escape sequence of single quote
+
+// alert("it's raining")  // alternative to it
+
+// alert("Welcome to 3MTT \n Class of 2024") //escape sequence of new line
+
+alert("\" 1 We shall soon be out of current hardship in Nigeria!\"") //double quote
+
+alert('" 2 We shall soon be out of current hardship in Nigeria!"')
+
