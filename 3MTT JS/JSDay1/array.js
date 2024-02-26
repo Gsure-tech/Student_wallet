2e43a79b005a51b56a3b1a34cb0bfc9926539f01
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-26 12:26:09 +0100

    using array methods

---
+++ 3MTT JS/JSDay1/array.js
@@ -0,0 +1,25 @@
+// alert('Array')
+
+let studentsName = ['Bimbo','Abdul','Sani','Moses'];
+
+let arrayLength = studentsName.length;
+let indexAbdul = studentsName.indexOf('Abdul');
+
+console.log("The length of the array is  " + arrayLength + " and the Index of Abdul is " + indexAbdul);
+
+
+
+let emptyArray = [];
+let name1, name2, name3, name4;
+
+name1 = prompt('Enter name 1');
+name1 = emptyArray.push(name1);
+
+name2 = prompt('Enter name 2');
+name2 = emptyArray.push(name2);
+
+name3 = prompt('Enter name 3');
+name3 = emptyArray.push(name3)
+
+name4 = prompt('Enter name 4');
+name4 = emptyArray.push(name4);
