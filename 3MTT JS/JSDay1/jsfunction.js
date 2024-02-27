574ff11c5041afd2bb47dc9cfb422636d10eaa12
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 11:52:09 +0100

    using arrow functions

--- 3MTT JS/JSDay1/jsfunction.js
+++ 3MTT JS/JSDay1/jsfunction.js
@@ -1,5 +1,34 @@
-const sayHello = function(name){
-    alert("Hello " + name);
+// const sayHello = function(name){
+//     alert("Hello " + name);
+// }
+// let person = prompt("What is your name")
+
+
+// function sayHello(name){
+//     alert("Hello " + name)
+// }
+// sayHello(person);
+
+//ARROW FUNCTION
+// let add =(a,b) => a+b;
+// let a=4;
+// let b =5;
+// let result1 = add(a,b);
+// alert(result1);
+
+
+let action = (a,b) => {
+    if(a>b){
+        alert(a + " is the greatest")
+    }else{
+        alert(b + " is the greatest")
+    }
 }
 
-sayHello("Abdulganiyu");
+let a,b;
+
+a = Number(prompt('Enter the value of a'));
+b = Number(prompt('Enter the value of b'));
+
+action(a,b)
+
