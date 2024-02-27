d165b233852393f6e634f1c0b471fd1ca818c31d
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 10:46:07 +0100

    using loop

---
+++ 3MTT JS/JSDay1/loopscript.js
@@ -0,0 +1,16 @@
+// alert('loop')
+// Syntax
+/*  
+for(initialization; condition; increment){
+    statement
+}
+*/
+
+let result, i, arrayOutput=[];
+
+for(i=1; i<=10; i++){
+    // result = arrayOutput.push(i)
+
+    
+    console.log(result);
+}
