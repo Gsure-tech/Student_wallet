f99018eea3935f278fdeda01422e31f7eea460b1
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 11:09:53 +0100

    using while loop

--- 3MTT JS/JSDay1/loopscript.js
+++ 3MTT JS/JSDay1/loopscript.js
@@ -23,3 +23,22 @@ for(i=1; i<=10; i++){
 
 console.log(result);
 console.log(odd)
+
+// using while loop
+
+let a =1, oddNumbers =[], evenNumbers =[];
+
+while(a<=10){
+
+        if(a % 2 !=0){
+            oddNumbers.push(a);
+            
+        } else{
+            evenNumbers.unshift(a);
+        }
+        a++;
+}
+
+
+console.log(evenNumbers);
+console.log(oddNumbers);
