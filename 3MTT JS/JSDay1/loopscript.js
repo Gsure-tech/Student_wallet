26920c6fd874ae7e1009feff5ac3c9579d491ac4
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 10:55:56 +0100

    using loop to get odd numbers

--- 3MTT JS/JSDay1/loopscript.js
+++ 3MTT JS/JSDay1/loopscript.js
@@ -1,16 +1,21 @@
-// alert('loop')
+alert('loop')
 // Syntax
 /*  
 for(initialization; condition; increment){
     statement
 }
 */
-
-let result, i, arrayOutput=[];
+ 
+// let result, i, arrayOutput=[];
+let result =[];
+let i;
 
 for(i=1; i<=10; i++){
     // result = arrayOutput.push(i)
+        if(i % 2 !=0){
+            result.push(i);
+        }
 
-    
-    console.log(result);
 }
+
+console.log(result);
