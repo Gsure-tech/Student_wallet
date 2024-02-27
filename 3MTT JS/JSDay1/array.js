837580fbe898a09e0612e5da0dc6e455dd7cedd4
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 10:29:43 +0100

    using the map method

--- 3MTT JS/JSDay1/array.js
+++ 3MTT JS/JSDay1/array.js
@@ -12,20 +12,26 @@ console.log("The length of the array is  " + arrayLength + " and the Index of Ab
 let emptyArray = [];
 let name1, name2, name3, name4;
 
-name1 = prompt('Enter name 1');
-emptyArray.push(name1);
+// name1 = prompt('Enter name 1');
+// emptyArray.push(name1);
 
-name2 = prompt('Enter name 2');
-emptyArray.push(name2);
+// name2 = prompt('Enter name 2');
+// emptyArray.push(name2);
 
-name3 = prompt('Enter name 3');
-emptyArray.push(name3)
+// name3 = prompt('Enter name 3');
+// emptyArray.push(name3)
 
-name4 = prompt('Enter name 4');
-emptyArray.push(name4);
+// name4 = prompt('Enter name 4');
+// emptyArray.push(name4);
 
 console.log(emptyArray);
 
 let numbers =[1,2,4,4,5,6,7];
 let result = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
-alert("The sum of the array is "+result)
+// alert("The sum of the array is "+result)
+
+let number2 =[1,2,3,4,5,7];
+let output = number2.map(num => num * num);
+
+alert(output);
+console.log(output);
