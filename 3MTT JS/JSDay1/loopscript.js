435a9f8296273263c86dc585692344ab3487b4cc
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 11:01:33 +0100

    using loop to get even numbers

--- 3MTT JS/JSDay1/loopscript.js
+++ 3MTT JS/JSDay1/loopscript.js
@@ -8,14 +8,18 @@ for(initialization; condition; increment){
  
 // let result, i, arrayOutput=[];
 let result =[];
+let odd =[];
 let i;
 
 for(i=1; i<=10; i++){
     // result = arrayOutput.push(i)
         if(i % 2 !=0){
             result.push(i);
+        } else{
+            odd.unshift(i);
         }
 
 }
 
 console.log(result);
+console.log(odd)
