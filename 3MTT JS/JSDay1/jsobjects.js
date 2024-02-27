98142b1b8625c79cc30d6852338a15c0b9339fc7
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-27 12:22:38 +0100

    using Object

---
+++ 3MTT JS/JSDay1/jsobjects.js
@@ -0,0 +1,16 @@
+let person = {
+    name : 'John',
+    age : 30,
+    city : 'New York'
+}
+
+// console.log(person)
+// console.log(person.name + " is " + person.age + " years old  and he is from " + person.city)
+
+let visitor = new Object();
+
+visitor.name = prompt('name');
+visitor.age = prompt('age');
+visitor.city = prompt ('city')
+
+console.log(visitor)
