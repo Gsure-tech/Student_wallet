ae38ae5211eedbb90d4e95d28423a62421ac35f9
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-29 11:34:22 +0100

    using div text content

--- 3MTT JS/3MTT JS DAY 4/main.js
+++ 3MTT JS/3MTT JS DAY 4/main.js
@@ -2,25 +2,38 @@
 
 const clickme = document.getElementById('clickme');
 const display = document.getElementById('display');
+const clickme2 = document.querySelector('#clickme2');
+const counterResult = document.querySelector('#counterResult');
 
 clickme.onclick = function(){
     // alert("I have been clicked");
     display.value ='Testing display'
 }
 
+clickme.addEventListener('click', function(){
+    display.value = i++;
+})
+
+clickme2.addEventListener('click', function(){
+    display.value = '';
+})
+
 // Counter
 // const increment 
 
 let i =0;
 function increment(){
-    display.value = i++;
+    counterResult.textContent = i++;
+    // display.value = i++;
 }
 function decrement(){
-    display.value = i--;
+    counterResult.textContent = i--;
+    // display.value = i--;
 }
 
 function reset(){
-    display.value = '';
+    counterResult.textContent = 0;
+    // display.value = '';
 }
 
 // display.onclick = function(){
