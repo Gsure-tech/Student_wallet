bac7ff58a41e37f8972fdfb712da4b458054d3db
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-02-29 10:56:46 +0100

    using the getElementById

--- 3MTT JS/3MTT JS DAY 4/main.js
+++ 3MTT JS/3MTT JS DAY 4/main.js
@@ -1,1 +1,21 @@
-alert("hi");
+// alert("hi");
+
+const clickme = document.getElementById('clickme');
+const display = document.getElementById('display');
+
+clickme.onclick = function(){
+    // alert("I have been clicked");
+    display.value ='Testing display'
+}
+
+
+
+
+
+
+// display.onclick = function(){
+// display.value ='Testing display'
+// }
+
+
+
