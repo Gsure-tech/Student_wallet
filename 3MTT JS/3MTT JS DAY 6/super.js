a47e584a5a1e469e0934c5b99831ab8834a89cda
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-03-07 11:25:48 +0100

    using  the super class

--- 3MTT JS/3MTT JS DAY 6/super.js
+++ 3MTT JS/3MTT JS DAY 6/super.js
@@ -1,16 +1,30 @@
 
 class Animal{
 
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
     eat(){
-        console.log('It can eat')
+        console.log(`${this.name} can eat`);
     }
     sleep(){
-        console.log('it can sleep')
+        console.log(`${this.name} can sleep`)
+    }
+    reproduce(){
+        console.log(`${this.name} can reproduce`)
     }
 }
 
 
 class Dog extends Animal{
+    constructor(name, age, runSpeed){
+        super(name, age);
+        this.runSpeed = runSpeed;
+
+    }
+
     run(){
         console.log('it can run')
     }
@@ -18,12 +32,22 @@ class Dog extends Animal{
 }
 
 class Fish extends Animal{
+    constructor(name, age, swimSpeed){
+        super(name, age);
+        this.swimSpeed = swimSpeed;
+
+    }
         swim(){
             console.log('it can swim')
         }
 }
 
 class Hawk extends Animal{
+    constructor(name, age, flySpeed){
+        super(name, age);
+        this.flySpeed = flySpeed;
+
+    }
         fly(){
             console.log('It can fly')
         }
