2b74b67cbcdafed2c0403c03388a58f9945e0ed2
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-03-18 10:36:36 +0100

    added the get all users endpoint

--- 3MTT NODE/begin/app.js
+++ 3MTT NODE/begin/app.js
@@ -1,38 +1,57 @@
-// const http = require('http');
-const expressPk = require("express");
+// // const http = require('http');
+// const expressPk = require("express");
 
-const app = expressPk();
+// const app = expressPk();
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome Home");
+// });
+
+// app.get("/getUsers", (req, res) => {
+//   res.send("Shibuzor \n ChiAmaka   \n Zahrah    \n Auwal");
+// });
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// // The create server requires 2 parameters which are the  request and the response:
+
+// // const server = http.createServer((req, res)=>{
+// //     res.statusCode = 200;
+// //     res.setHeader('Content-Type', 'text/plain');
+// //     res.end('Hello, World! \n')
+// // })
+// /**
+//  * Cereal God
+//  *
+//  * create endpoints to getUsers
+//  * about
+//  * an endpoint that will return all of the names of 3mtt users
+//  */
+
+// app.listen(port, () => {
+//   console.log("Server is running on portf");
+// });
+
+// // server.listen(port, hostname, ()=>{
+// //     console.log('Server runnimg at http:/127.0.0.1:3000')
+// // })
+
+
+const express = require("express");
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome Home");
 });
 
 app.get("/getUsers", (req, res) => {
-  res.send("Shibuzor \n ChiAmaka   \n Zahrah    \n Auwal");
+  const users = ["Shibuzor", "ChiAmaka", "Zahrah", "Musa"];
+  res.send(users);
 });
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
-// The create server requires 2 parameters which are the  request and the response:
-
-// const server = http.createServer((req, res)=>{
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello, World! \n')
-// })
-/**
- * Cereal God
- *
- * create endpoints to getUsers
- * about
- * an endpoint that will return all of the names of 3mtt users
- */
-
 app.listen(port, () => {
-  console.log("Server is running on portf");
+  console.log("Server is running on port " + port);
 });
-
-// server.listen(port, hostname, ()=>{
-//     console.log('Server runnimg at http:/127.0.0.1:3000')
-// })
