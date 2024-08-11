a059e84e8985364e92613cb34d0d7d4c7109640e
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-08-11 18:36:41 +0100

    created variables

--- myJSLearn/p1/script.js
+++ myJSLearn/p1/script.js
@@ -1,4 +1,22 @@
-let js = 'amazing';
-if(js == 'amazing') alert('JavaScript is FUN!')
-
 console.log (40 + 8 + 23 - 10);
+console.log('Jonas');
+
+// Values and variables
+console.log(23)
+
+let firstName = "Matilda";
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+// Conventions of naming varibales
+/* *
+1. camelCase   firstName
+2. cannot start with a number eg let 3years = 3;
+3. Only contain a numbers letters underscore or dollar sign
+4. Cannot contain a reserved Javascript word
+5. should not start with an uppercase except constants which is in all UPPERCASE
+
+*/
+
+let PI = 3.1415;
